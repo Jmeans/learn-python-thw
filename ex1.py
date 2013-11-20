b54a4353dd5304a9de1cfcab00c424e12e091829
Jenn Slemboski <jslemboski@blendtec.com>
2013-11-20 09:11:33 -0700

    initial commit

---
+++ ex1.py
@@ -0,0 +1,8 @@
+Print "Hello World!"
+Print "Hello Again"
+Print "I like typing this."
+Print "This is fun."
+Print 'Yay! Printing.'
+Print "I'd much rather you 'not'."
+Print 'I "said" do not touch this.'
+#"Finally."
